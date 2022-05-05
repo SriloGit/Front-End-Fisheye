c49485430fe2efe85f2b9bc4589deb146aacd8eb
SriloGit <chambonjeremy2@gmail.com>
2022-05-05 14:12:59 +0200

    esLint install

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,27 +1,29 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
 const getPhotographers = async () => {
-    const { photographers } = await (
-      fetch('./data/photographers.json')
-        .then((response) => response.json())
-    )
-    return { photographers }
-  }
-  
-  async function displayData (photographers) {
-    const photographersSection = document.querySelector('.photographer_section')
-  
-    photographers.forEach((photographer) => {
-      const photographerModel = photographerFactory(photographer)
-      const userCardDOM = photographerModel.getUserCardDOM()
-      photographersSection.appendChild(userCardDOM)
-    })
-  };
-  
-  async function init () {
-    // Get photograph data
-    const { photographers } = await getPhotographers()
+  const { photographers } = await (
+    fetch('./data/photographers.json')
+      .then((response) => response.json())
+  );
+  return { photographers };
+};
 
-    // HTML Layout
-    await displayData(photographers)
-  };
-  
-  init()
+async function displayData(photographers) {
+  const photographersSection = document.querySelector('.photographer_section');
+
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+async function init() {
+  // Get photograph data
+  const { photographers } = await getPhotographers();
+
+  // HTML Layout
+  await displayData(photographers);
+}
+
+init();
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,106 +1,105 @@
-async function getPhotographer () {
+/* eslint-disable eqeqeq */
+/* eslint-disable max-len */
+/* eslint-disable no-undef */
+async function getPhotographer() {
   // Get Id from URL
-  const id = window.location.search.split('id=')[1]
+  const id = window.location.search.split('id=')[1];
 
   const photographerData = await (
     fetch('./data/photographers.json')
       .then((response) => response.json())
-  )
+  );
 
-  photographerData.photographers = photographerData.photographers.filter((photographer) => photographer.id == id)[0]
-  photographerData.media = photographerData.media.filter((media) => media.photographerId == id)
+  // eslint-disable-next-line prefer-destructuring
+  photographerData.photographers = photographerData.photographers.filter((photographer) => photographer.id == id)[0];
+  photographerData.media = photographerData.media.filter((media) => media.photographerId == id);
 
-  return photographerData
+  return photographerData;
 }
 
 // display data dynamically for each photographer
-function displayPhotographerData (photographer) {
-  const photographersSection = document.getElementById('photographeInfos')
-  const profileModel = photographerFactory(photographer)
-  const profiles = profileModel.getProfilePage()
-  photographersSection.appendChild(profiles)
-};
+function displayPhotographerData(photographer) {
+  const photographersSection = document.getElementById('photographeInfos');
+  const profileModel = photographerFactory(photographer);
+  const profiles = profileModel.getProfilePage();
+  photographersSection.appendChild(profiles);
+}
 
 // display media data dynamically for each photographer
-function displayMediaData (photographerMedias) {
-  const photographersSection = document.getElementById('photographeMedias')
+function displayMediaData(photographerMedias) {
+  const photographersSection = document.getElementById('photographeMedias');
 
-  photographersSection.innerHTML = ''
+  photographersSection.innerHTML = '';
 
   photographerMedias.forEach((media) => {
-    const mediasModel = photographerFactory(media)
-    const medias = mediasModel.getMediaPage()
-    photographersSection.appendChild(medias)
-  })
+    const mediasModel = photographerFactory(media);
+    const medias = mediasModel.getMediaPage();
+    photographersSection.appendChild(medias);
+  });
 
-  //Get the number of likes of every medias
-  const totalLikeModel = photographerFactory(photographerMedias)
+  // Get the number of likes of every medias
+  const totalLikeModel = photographerFactory(photographerMedias);
   totalLikeModel.getTotalLikes();
-  
-};
+}
 
-function displayNameContactData (photographerContact){
-  const photographerName = document.querySelector('.contact-header')
-  const contactModel = photographerFactory(photographerContact)
-  const contact = contactModel.getContactNameDOM()
-  photographerName.appendChild(contact)
-};
+function displayNameContactData(photographerContact) {
+  const photographerName = document.querySelector('.contact-header');
+  const contactModel = photographerFactory(photographerContact);
+  const contact = contactModel.getContactNameDOM();
+  photographerName.appendChild(contact);
+}
 
-
-//Init all function
-async function init () {
+// Init all function
+async function init() {
   // Photograph data
-  const photographerData = await getPhotographer()
+  const photographerData = await getPhotographer();
 
   // Utils object
-  const photographerUtils = new PhotographerUtils()
+  const photographerUtils = new PhotographerUtils();
 
   // Add every medias of the photograph to an array to sort them
-  let mediaArray = photographerData.media
+  let mediaArray = photographerData.media;
 
   // HTML Layout
-  displayPhotographerData(photographerData.photographers)
-  mediaArray = photographerUtils.sortMedia('popularite', mediaArray)
-  displayMediaData(mediaArray)
-  displayNameContactData(photographerData.photographers)
+  displayPhotographerData(photographerData.photographers);
+  mediaArray = photographerUtils.sortMedia('popularite', mediaArray);
+  displayMediaData(mediaArray);
+  displayNameContactData(photographerData.photographers);
 
   // Event to sort media
-  document.getElementById('dropdown').addEventListener('click', function (e) {
-  document.getElementById('sort-button-content').innerHTML = e.target.innerHTML
-    mediaArray = photographerUtils.sortMedia(e.target.id, mediaArray)
-    displayMediaData(mediaArray)
-  })
+  document.getElementById('dropdown').addEventListener('click', (e) => {
+    document.getElementById('sort-button-content').innerHTML = e.target.innerHTML;
+    mediaArray = photographerUtils.sortMedia(e.target.id, mediaArray);
+    displayMediaData(mediaArray);
+  });
 
   // Event to add likes
-  document.addEventListener('click', function (e) {
+  document.addEventListener('click', (e) => {
     if (e.target.classList.contains('incrementLike')) {
-      photographerUtils.incrementLike(e.target)
+      photographerUtils.incrementLike(e.target);
     }
   });
 
   // Init Lightbox on click
 
-  document.addEventListener('click', function (e) {
-
+  document.addEventListener('click', (e) => {
     if (e.target.classList.contains('lightboxMedia')) {
-      photographerUtils.initLightbox()
+      photographerUtils.initLightbox();
     }
-  })
-  //Using keyboard for lightbox
-  document.addEventListener('keydown', function (event)
-  {
+  });
+  // Using keyboard for lightbox
+  document.addEventListener('keydown', (event) => {
     if (event.target.classList.contains('lightboxMedia')) {
-      photographerUtils.arrowKey(event)
+      photographerUtils.arrowKey(event);
     }
-  })
+  });
 
-  //Show form value in the console
-  document.getElementById('send').addEventListener('click', function (e)
-  {
+  // Show form value in the console
+  document.getElementById('send').addEventListener('click', (e) => {
     e.preventDefault();
-    console.log(document.getElementById('firstname').value, document.getElementById('lastname').value, document.getElementById('email').value,  )
-  })
+    // eslint-disable-next-line no-console
+    console.log(document.getElementById('firstname').value, document.getElementById('lastname').value, document.getElementById('email').value);
+  });
+}
 
-};
-
-  init()
+init();
